341f2af784d15c4eb47b268ee9e5414a6bfd7372
yohannesr2 <yohannesr2@yahoo.com>
2020-04-02 20:26:30 -0600

    commit from robel

---
+++ classofRobel/src/classofRobel/Firstjava.java
@@ -0,0 +1,10 @@
+package classofRobel;
+
+public class Firstjava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(" greeting from the mile high city");
+	}
+
+}
